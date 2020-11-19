91d4f9bdf33a010bd3ce50e97a8d0eefecb94b78
TomJSoftwire <thomas.johnston@softwire.com>
2020-11-19 15:37:48 +0000

    Error Logs added

---
+++ src/main/java/training/supportbank/JSONReader.java
@@ -0,0 +1,11 @@
+package training.supportbank;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+public class JSONReader extends Reader {
+    public List<Transaction> readFile(String filename) throws IOException{
+
+    }
+}
