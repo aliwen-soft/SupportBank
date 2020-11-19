ae4ff8e9072ef2d2f81bbf1f163cbd110fbe8f05
TomJSoftwire <thomas.johnston@softwire.com>
2020-11-19 14:05:58 +0000

    Updated Transaction.java class

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,14 +1,16 @@
 package training.supportbank;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        try {
-            CSVReader.readFile("Transactions2014.csv");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //Initialise Bank object
+        //Read input files and generate transaction list & person list
+        //Feed transaction lists to bank
+        //bank will have listAll and list(personAccount)
+        UserInput.run();
+
         System.out.println("Test!");
     }
 }
