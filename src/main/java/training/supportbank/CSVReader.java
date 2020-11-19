7e2c4c4892b1aebbbfed8af8afb68d249d97b629
TomJSoftwire <thomas.johnston@softwire.com>
2020-11-19 15:22:30 +0000

    Error Logs added

--- src/main/java/training/supportbank/CSVReader.java
+++ src/main/java/training/supportbank/CSVReader.java
@@ -1,5 +1,8 @@
 package training.supportbank;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -9,14 +12,14 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class CSVReader implements Reader {
-
+    private static final Logger LOGGER = LogManager.getLogger();
     public List<Person> people;
 
     public CSVReader(List<Person> people){
         this.people=people;
     }
 
-    public List<Transaction> readFile(String filename) throws IOException {
+    public List<Transaction> readFile(String filename) throws IOException,NumberFormatException {
         List<Transaction> transactions = new ArrayList<Transaction>();
 
         BufferedReader csvReader = new BufferedReader(new FileReader(filename));
@@ -35,8 +38,10 @@ public class CSVReader implements Reader {
                     Transaction transaction= new Transaction(date,amount,from,to,narrative);
                     transactions.add(transaction);
                 } catch (ParseException e) {
-                    e.printStackTrace();
-                }
+                    LOGGER.info("Invalid date: " + data[0]);
+                } catch(NumberFormatException e){
+                LOGGER.info("Non-parsable string given for amount: '" + data[4] + "'");
+            }
             }
             first=false;
         }
--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,13 +1,18 @@
 package training.supportbank;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+    private static final Logger LOGGER = LogManager.getLogger();
     public static void main(String args[]) {
+
         //Initialise Bank object
         Bank bank = new Bank();
-        bank.updateTransactionFromFile("Transactions2014.csv");
+        bank.updateTransactionFromFile("DodgyTransactions2015.csv");
         //Read input files and generate transaction list & person list
         //Feed transaction lists to bank
         //bank will have listAll and list(personAccount)
