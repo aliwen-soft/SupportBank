9c28817632fc98a51754337ffe894f5488d26b7c
Alice Wenban <Alice.Wenban@softwire.com>
2020-11-19 12:01:26 +0000

    Added if basic template for reader

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,8 +1,14 @@
 package training.supportbank;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String args[]) {
-        // Your code here!
+        try {
+            CSVReader.readFile("Transactions2014.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println("Test!");
     }
 }
