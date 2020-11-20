10e2e1ea336bb79c152ced7258b1be926fa73869
Alice Wenban <Alice.Wenban@softwire.com>
2020-11-20 16:07:32 +0000

    money now .nn

--- src/main/java/training/supportbank/Money.java
+++ src/main/java/training/supportbank/Money.java
@@ -7,63 +7,64 @@ public class Money {
     private static final Logger LOGGER = LogManager.getLogger();
     int amount;
 
-    public Money(int amount){
-        this.amount=amount;
-    }
-    public Money(double amount){
-        this.amount= (int) Math.round(amount*100);
-    }
-    public Money(String samount) throws NumberFormatException{
-                double amount = Double.parseDouble(samount);
-                this.amount = (int) Math.round(amount * 100);
-
-
+    public Money(int amount) {
+        this.amount = amount;
     }
 
-    public String printMoney(){
-        double dbl = amount/100;
-        String printamount= Double.toString(dbl);
-        return  "£ "+printamount;
+    public Money(double amount) {
+        this.amount = (int) Math.round(amount * 100);
     }
-    public String printMoney(boolean sign){
-        double dbl = amount/100;
-        String printamount= Double.toString(dbl);
-        if (sign) {
-            return "£ " + printamount;
+
+    public Money(String samount) throws NumberFormatException {
+        double amount = Double.parseDouble(samount);
+        this.amount = (int) Math.round(amount * 100);
+    }
+
+    public String printMoney(boolean sign) {
+        double dbl = (double) amount / 100;
+        String printAmount = Double.toString(dbl);
+        while(printAmount.substring(printAmount.lastIndexOf(".") + 1).length()<2){
+              printAmount=printAmount+"0";
+          }
+        if(sign) {
+            return "£" + printAmount;
         }else{
-            return printamount;
+            return printAmount;
         }
     }
 
-    public int getAmount(){
+
+    public int getAmount() {
         return amount;
     }
 
-    public void setAmount(int amount){
-        this.amount= amount;
-    }
-    public void setAmount(double amount){
-        this.amount= (int) Math.round(amount*100);
+    public void setAmount(int amount) {
+        this.amount = amount;
     }
 
-    public int add(int amount){
-        this.amount= this.amount+amount;
-        return this.amount;
+    public void setAmount(double amount) {
+        this.amount = (int) Math.round(amount * 100);
     }
-    public int add(Money money){
-        this.amount= this.amount+money.amount;
+
+    public int add(int amount) {
+        this.amount = this.amount + amount;
         return this.amount;
     }
 
-    public int sub(int amount){
-        this.amount= this.amount-amount;
-        return this.amount;
-    }
-    public int sub(Money money){
-        this.amount= this.amount-money.amount;
+    public int add(Money money) {
+        this.amount = this.amount + money.amount;
         return this.amount;
     }
 
+    public int sub(int amount) {
+        this.amount = this.amount - amount;
+        return this.amount;
+    }
+
+    public int sub(Money money) {
+        this.amount = this.amount - money.amount;
+        return this.amount;
+    }
 
 
 }
