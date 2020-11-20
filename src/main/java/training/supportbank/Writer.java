bbd6d3a3cb18e04d217f3ec2ac6e70b7cb0d428d
Alice Wenban <Alice.Wenban@softwire.com>
2020-11-20 09:14:58 +0000

    writer csv complete

---
+++ src/main/java/training/supportbank/Writer.java
@@ -0,0 +1,13 @@
+package training.supportbank;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+public abstract class Writer {
+    public static DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+    public abstract void Write(List<Transaction> transactions, String file) throws IOException;
+
+}
