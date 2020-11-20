73c3ae86f4ec52c2190562510d7b793d3c2134d9
TomJSoftwire <thomas.johnston@softwire.com>
2020-11-20 15:28:45 +0000

    JSONReader update

--- src/main/java/training/supportbank/UserInput.java
+++ src/main/java/training/supportbank/UserInput.java
@@ -3,7 +3,22 @@ package training.supportbank;
 import java.util.Scanner;
 
 public class UserInput {
-    public static String helpMessage = "help info";
+    public static final String NEW_LINE = System.getProperty("line.separator");
+    public static String helpMessage =
+            "----- Support Bank v 1.0 -----"
+            + NEW_LINE
+            + "Available commands:"
+            + NEW_LINE
+            + "List All - Prints a list of all people and value of their account"
+            + NEW_LINE
+            + "List [name] - Prints all transactions associated with [name]"
+            + NEW_LINE
+            + "import [file] - imports transactions from [file]. File must be .JSON .CSV or .XML"
+            + NEW_LINE
+            + "export [file] - exports transactions to [file]. File must be .CSV"
+            + NEW_LINE
+            + "exit - exits program";
+
     public static void run(Bank bank) {
         boolean keepRunning = true;
         while(keepRunning) {
@@ -11,24 +26,27 @@ public class UserInput {
             System.out.print("Enter command:");
             String command = scanner.nextLine();
             String[] commands = command.split(" ");
-            String commandModifier;
 
                 if (commands[0].equals("List")) {
-                    try {
-                        if (commands[1].equals("All")) {
-                            System.out.println("listing all");
-                            bank.listAll();
-                        } else {
-                            String name="";
-                            for(int i=1;i<commands.length;i++){
-                                name=name+" "+commands[i];
+                    if(bank.getTransactions().size() == 0){
+                        System.out.println("No transactions imported");
+                    } else {
+                        try {
+                            if (commands[1].equals("All")) {
+                                System.out.println("listing all");
+                                bank.listAll();
+                            } else {
+                                String name = "";
+                                for (int i = 1; i < commands.length; i++) {
+                                    name = name + " " + commands[i];
+                                }
+                                name = name.trim();
+                                bank.listAccount(name);
+                                System.out.println("listing transactions for" + name);
                             }
-                            name = name.trim();
-                            bank.listAccount(name);
-                            System.out.println("listing transactions for" + name);
+                        } catch (StringIndexOutOfBoundsException e) {
+                            System.out.println("Can call 'list all' or 'list [Person]'");
                         }
-                    } catch (StringIndexOutOfBoundsException e) {
-                        System.out.println("Can call 'list all' or 'list [Person]'");
                     }
                 }else if (commands[0].equals("Import")){
                     System.out.println("Importing file");
